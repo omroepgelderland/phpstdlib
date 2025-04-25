e738a8f84d99ce0f00ea31cd12da69231c5554ee
Remy Glaser <rglaser@gld.nl>
2025-04-25 13:55:54 +0200

    PHP checkers paden
    Gebruik exclude ipv lijst met paden bij PHP checkers

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,7 +3,9 @@
 export COMPOSER_NO_DEV=0
 composer8.1 install || exit 1
 composer8.1 check-platform-reqs || exit 1
-php8.1 vendor/bin/parallel-lint src/ tests/ || exit 1
+php8.1 vendor/bin/parallel-lint \
+    --exclude vendor/ \
+    ./ || exit 1
 vendor/bin/phpstan analyse || exit 1
 php8.1 vendor/bin/phpcs --standard=ruleset.xml -n || exit 1
 php8.1 vendor/bin/phpunit tests || exit 1
